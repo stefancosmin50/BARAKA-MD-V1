2aa42e3f82fe6e8b7e3f3b54a70d8bf9dc697baa
cosmin <scosmin764@gmail.com>
2024-11-07 00:10:10 +0200

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -114050,7 +114050,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Your Session Id",
+  SESSION_ID: process.env.SESSION_ID || "Bmwmd$hSZzw2Ax",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
